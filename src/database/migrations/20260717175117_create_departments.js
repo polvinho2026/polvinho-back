d1144e766ae9a2885e82d47c4a9c5fc89e9ac8c3
ppachecoo <rineopacheco@gmail.com>
2026-07-20 15:45:48 -0300

    fix: preenche as migrations que foram enviadas vazias

--- src/database/migrations/20260717175117_create_departments.js
+++ src/database/migrations/20260717175117_create_departments.js
@@ -3,13 +3,26 @@
  * @returns { Promise<void> }
  */
 export function up(knex) {
-  
-};
+  return knex.schema.createTable('departments', (table) => {
+    
+    table.uuid('id').primary().defaultTo(knex.fn.uuid());
+    table.string('title', 100).notNullable();
+    table.specificType('entity_code', 'char(9)').unique().notNullable();
+    table.uuid('created_by')
+      .references('id')
+      .inTable('users')
+      .onDelete('SET NULL'); 
+
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+    table.timestamp('deleted_at').nullable().defaultTo(null);
+  });
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 export function down(knex) {
-  
-};
+  return knex.schema.dropTableIfExists('departments');
+}
--- src/database/migrations/20260720165517_crate_courses.js
+++ src/database/migrations/20260720165517_crate_courses.js
@@ -2,14 +2,32 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+export function up(knex) {
+  return knex.schema.createTable('courses', (table) => {
+ 
+    table.uuid('id').primary().defaultTo(knex.fn.uuid());
+    table.uuid('department_id')
+      .references('id')
+      .inTable('departments')
+      .onDelete('CASCADE'); 
+      
+    table.string('title', 100).notNullable();
+    table.specificType('entity_code', 'char(9)').unique().notNullable();
+    table.uuid('created_by')
+      .references('id')
+      .inTable('users')
+      .onDelete('SET NULL'); 
+
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+    table.timestamp('deleted_at').nullable().defaultTo(null);
+  });
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+export function down(knex) {
+  return knex.schema.dropTableIfExists('courses');
+}
