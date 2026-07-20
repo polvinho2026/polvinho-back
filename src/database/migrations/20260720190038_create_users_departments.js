031e62b3cbdc5f28d65f68eeeb288ed0687b4bdb
ppachecoo <rineopacheco@gmail.com>
2026-07-20 16:04:02 -0300

    migration users_department

---
+++ src/database/migrations/20260720190038_create_users_departments.js
@@ -0,0 +1,29 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('users_departments', (table) => {
+    
+    table.uuid('id').primary().defaultTo(knex.fn.uuid());
+    table.uuid('department_id')
+      .references('id')
+      .inTable('departments')
+      .onDelete('CASCADE'); 
+    table.uuid('user_id')
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE'); 
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+    table.timestamp('deleted_at').nullable().defaultTo(null);
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTableIfExists('users_departments');
+}
