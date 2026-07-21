2ba73bc347f5a9dc800d1bfd970d9219cdae398a
Fabricioosm <fabricioszczpaniak@gmail.com>
2026-07-21 11:54:26 -0300

    create assignments

---
+++ src/database/migrations/20260721133736_create-assignments.js
@@ -0,0 +1,33 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+    return knex.schema.createtable('assignments', (table) =>{
+        table.uuid('id').primary().defaultTo(knex.fn.uuid());
+        table.uuid('created_by')
+            .nullable()
+            .references('id')
+            .inTable('users')
+            .onDelete('SET NULL');
+        table.uuid('id_subject')
+            .references('id')
+            .inTable('subjects')
+            .onDelete('CASCADE');
+        table.string('title', 100).notNullable();
+        table.text('description').nullable();
+        table.date('date_start').nullable();
+        table.date('date_end').nullable();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+        table.timestamp('deleted_at').nullable();
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  
+};
