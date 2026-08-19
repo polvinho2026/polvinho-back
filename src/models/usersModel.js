abddfeeb1ba4b8cf6ae13dca0e43d0b1a5c02c20
ppachecoo <rineopacheco@gmail.com>
2026-08-19 11:52:57 -0300

    feat: implementa listagem de usuarios

--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -1,3 +1,5 @@
+// parte do backend responsável por fazer a comunicação com o banco de dados, realizando operações como criar, buscar e atualizar registros na tabela de usuários.
+
 import db from '../database/connection.js';
 
 const TABLE_NAME = 'users';
@@ -19,9 +21,54 @@ const findByRegistration = async (cpf) => {
     return await db(TABLE_NAME).where({ cpf }).first();
 };
 
+const list = async (params) => {
+    console.log('3. são essas informações que chegaram no model:', params);
+    const { 
+        page = 1,
+        name, 
+        registration, 
+        role,
+        includeExcluded = false,
+        //allowedUserIds
+    } = params;
+
+// define o limite de registros por página
+    const limit = 20; 
+    const offset = (page - 1) * limit;
+
+// buscando no banco. 
+    const query = db(TABLE_NAME).select('name', 'registration', 'role');
+    if (!includeExcluded) {
+    query.whereNull('deleted_at'); 
+  }
+
+  if (name) {
+    query.where('name', 'ilike', `%${name}%`); 
+  }
+  if (registration) {
+    query.where('registration', 'ilike', `%${registration}%`);
+  }
+
+  if (role) {
+    query.where('role', role);
+  }
+
+//   if (allowedUserIds) {
+//     query.whereIn('id', allowedUserIds);
+//   }
+
+  query.limit(limit).offset(offset);
+
+  console.log('4. dados retornados do model:', await query.toString());
+
+  return await query;
+};
+
 export default {
     create,
     findByEmail,
     findByCPF,
-    findByRegistration
+    findByRegistration,
+    list
 };
+
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -4,3 +4,6 @@ import usersController from "../controllers/usersController.js";
 export const usersRoutes = Router();
 
 usersRoutes.post('/', usersController.create);
+usersRoutes.get('/', usersController.list);
+
+export default usersRoutes;
