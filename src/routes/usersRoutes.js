ada80686fefe92ad8c1a4ab8e76feddbf565ebec
ppachecoo <rineopacheco@gmail.com>
2026-09-04 14:58:54 -0300

    feat: implementa fluxo completo de edicao de usuario no backend

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -6,5 +6,6 @@ export const usersRoutes = Router();
 usersRoutes.post('/', usersController.create);
 usersRoutes.get('/', usersController.list);
 usersRoutes.get('/:id', usersController.show);
+usersRoutes.put('/:id', usersController.update);
 
 export default usersRoutes;
